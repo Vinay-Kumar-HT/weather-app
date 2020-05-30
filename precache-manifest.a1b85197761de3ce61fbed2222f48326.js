b5290140b55dc814ddacc684e85437930402607f
vinay acharya <htvinaykumar98@gmail.com>
2020-05-30 22:13:50 +0530

    Updates

---
+++ precache-manifest.a1b85197761de3ce61fbed2222f48326.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b5b9f49b5b2463e5d7d3ca9e365004a",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "7a00b574b73642b3010c",
+    "url": "/weather-app/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "f93a34a30d9859292aae",
+    "url": "/weather-app/static/js/2.4ab226f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.4ab226f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a00b574b73642b3010c",
+    "url": "/weather-app/static/js/main.1f40ba25.chunk.js"
+  },
+  {
+    "revision": "12ef64cf8e1aad5a2dd2",
+    "url": "/weather-app/static/js/runtime-main.c90f5b59.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/weather-app/static/media/logo.ee7cd8ed.svg"
+  }
+]);
