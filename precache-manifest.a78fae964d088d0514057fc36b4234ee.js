70de7438affa38928a2b43bfc4d9f2bf7b51a852
vinay acharya <htvinaykumar98@gmail.com>
2020-05-30 21:50:39 +0530

    Updates

---
+++ precache-manifest.a78fae964d088d0514057fc36b4234ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4fab1113cf3b439c5f1340b537bcbec",
+    "url": "/weather-app.git/index.html"
+  },
+  {
+    "revision": "c297c820da5ace22d8d9",
+    "url": "/weather-app.git/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "f93a34a30d9859292aae",
+    "url": "/weather-app.git/static/js/2.4ab226f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app.git/static/js/2.4ab226f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c297c820da5ace22d8d9",
+    "url": "/weather-app.git/static/js/main.a5528c57.chunk.js"
+  },
+  {
+    "revision": "e21727762c2573c5f635",
+    "url": "/weather-app.git/static/js/runtime-main.5b834fae.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/weather-app.git/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/weather-app/precache-manifest.db6e2ce82111ed7077165deafcaa2dea.js"
+  "/weather-app.git/precache-manifest.a78fae964d088d0514057fc36b4234ee.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/weather-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/weather-app.git/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
